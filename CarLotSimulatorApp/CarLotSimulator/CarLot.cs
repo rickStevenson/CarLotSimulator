9f185511b1c968caee9608fdc21aa7210e580a7c
RickStevenson <dadsathome1@gmail.com>
2024-09-10 14:18:49 -0600

    Finished Exercise 2

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class CarLot
+    {
+        //Create a CarLot class
+        //It should have at least one property: a List of cars
+        //Instantiate a Carlot at the beginning of the program and as you create a car add the car to the list.
+        //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+        public List<Car> Cars { get; set; } = new List<Car>();
+
+        public void AddCar(Car car)
+        {
+            Cars.Add(car);
+        }
+        public void PrintCarDetails()
+        {
+            foreach (var car in Cars)
+            {
+                Console.WriteLine($"A {car.Year} {car.Make} {car.Model}");
+            }
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace CarLotSimulator
 {
@@ -28,6 +29,52 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            var carLot = new CarLot();
+
+            Console.WriteLine("Welcome to Fast Eddie's Car Lot!");
+            Console.WriteLine("________________________________");
+
+            var car1 = new Car()
+            {
+                Year = 2020,
+                Make = "Chevy",
+                Model = "Camero",
+                EngineNoise = "rev",
+                HonkNoise = "mild",
+                IsDriveable = true
+            };
+            Console.WriteLine($"This is a {car1.Year} {car1.Make} {car1.Model}");
+            car1.MakeEngineNoise(car1.EngineNoise);
+            car1.MakeHonkNoise(car1.HonkNoise);
+            Console.WriteLine("");
+
+            var car2 = new Car();
+
+            car2.Year = 2022;
+            car2.Make = "Ford";
+            car2.Model = "Mustang";
+            car2.EngineNoise = "purr";
+            car2.HonkNoise = "loud";
+            car2.IsDriveable = true;
+            Console.WriteLine($"This is a {car2.Year} {car2.Make} {car2.Model}");
+            car2.MakeEngineNoise(car2.EngineNoise);
+            car2.MakeHonkNoise(car2.HonkNoise);
+            Console.WriteLine("");
+
+            var car3 = new Car(2024, "Toyota", "4 Runner", "purr", "mild", true);
+            Console.WriteLine($"This is a {car3.Year} {car3.Make} {car3.Model}");
+            car3.MakeEngineNoise(car3.EngineNoise);
+            car3.MakeHonkNoise(car3.HonkNoise);
+
+            carLot.AddCar(car1);
+            carLot.AddCar(car2);
+            carLot.AddCar(car3);
+
+            Console.WriteLine("");
+            Console.WriteLine("__________________");
+            Console.WriteLine("You viewed the following cars:");
+            carLot.PrintCarDetails();
         }
     }
 }
